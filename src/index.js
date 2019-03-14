1de8c721f74ff6ac92de389aba0d7ef3d8034013
Artyom Titanov <artitanov@gmail.com>
2019-03-15 00:15:12 +0300

    feat: ready index.js. 20 of 20 test passed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  arrayOFstr = str.split('');
+    if (arrayOFstr.length % 2 != 0) {return false} else {
+      for (i=0; i<arrayOFstr.length;i++){
+        for (k=0; k<bracketsConfig.length; k++){
+          if (arrayOFstr[i] === bracketsConfig[k][0] && arrayOFstr[i+1] === bracketsConfig[k][1]) {
+            arrayOFstr.splice(i,2);
+            i = -1;
+          }
+        }
+      }
+    }
+    if (arrayOFstr.length>0){return false;} else {return true};
 }
+
